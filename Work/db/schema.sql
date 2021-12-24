f61b0e9d5ab3e4fe4477e26334d972f977bfba09
Cameron White <whitcam20@gmail.com>
2021-12-23 23:10:38 -0500

    rewriting with cascade

--- Work/db/schema.sql
+++ Work/db/schema.sql
@@ -1,24 +1,25 @@
---set up database
+-- set up database
 DROP DATABASE IF EXISTS business_db;
 CREATE DATABASE business_db;
 USE business_db;
 
---set up tables(departments, roles, employees)
+-- set up tables(departments, roles, employees)
+
 DROP TABLE IF EXISTS department;
 CREATE TABLE department (
-    name VARCHAR(30) NOT NULL,
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    department_name VARCHAR(30) NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY
 );
 
 DROP TABLE IF EXISTS role;
 CREATE TABLE role (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, --role id primary key
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INT NOT NULL, --role department_id foreign key
+    department_id INT,
     FOREIGN KEY (department_id)
         REFERENCES department(id)
-        ON DELETE SET NULL
+        ON DELETE CASCADE
 );
 
 DROP TABLE IF EXISTS employee;
@@ -26,12 +27,10 @@ CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL,
-    manager_id INT NOT NULL,
+    role_id INT,
+    manager_id INT,
     FOREIGN KEY (role_id)
         REFERENCES role(id)
-        ON DELETE SET NULL,
-    FOREIGN KEY (manager_id) 
-        REFERENCES employee(id)
-        ON DELETE SET NULL
+        ON DELETE CASCADE
+   
 );
